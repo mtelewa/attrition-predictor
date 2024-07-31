69f5afa74a03c515a07ce1b5c8ac816e12834a6f
Mohamed Tarek <m.tarek@live.co.uk>
2024-07-31 04:12:02 +0000

    add helper function for model evaluation

---
+++ src/machine_learning/evaluate_clf.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+from sklearn.metrics import classification_report, confusion_matrix
+
+def confusion_matrix_and_report(X, y, pipeline, label_map):
+    """
+    Display the confusion matrix and report
+    """
+
+    prediction = pipeline.predict(X)
+
+    st.write('#### Confusion Matrix')
+    st.code(pd.DataFrame(confusion_matrix(y_true=prediction, y_pred=y),
+                         columns=[["Actual " + sub for sub in label_map]],
+                         index=[["Prediction " + sub for sub in label_map]]
+                         ))
+
+    st.write('#### Classification Report')
+    st.code(classification_report(y, prediction, target_names=label_map), "\n")
+
+
+def clf_performance(X_train, y_train, X_test, y_test, pipeline, label_map):
+    """
+    Pretty printing function for the confusion_matrix_and_report function
+    """
+    st.info("Train Set")
+    confusion_matrix_and_report(X_train, y_train, pipeline, label_map)
+
+    st.info("Test Set")
+    confusion_matrix_and_report(X_test, y_test, pipeline, label_map)
