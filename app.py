bb7ff936c6b1392adfeaeb35298bb9810a7f9630
Mohamed Tarek <m.tarek@live.co.uk>
2024-07-31 08:11:07 +0000

    add project conclusion streamlit page

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from app_pages.page_hyptheses import page_hypotheses_body
 from app_pages.page_correlation_analysis import page_correlation_body
 from app_pages.page_attrition_predictor import page_attrition_predictor_body
 from app_pages.page_model_performance import page_model_performance_body
-from app_pages.page_project_summary import page_project_summary_body
+from app_pages.page_conclusion import page_conclusion_body
 
 app = MultiPage(app_name= "Attrition Predictor") # Create an instance of the app 
 
@@ -17,6 +17,6 @@ app.add_page("Project Hypotheses", page_hypotheses_body)
 app.add_page("Project Correlation Analysis", page_correlation_body)
 app.add_page("Attrition Predictor", page_attrition_predictor_body)
 app.add_page("Model Performance", page_model_performance_body)
-app.add_page("Project Summary", page_project_summary_body)
+app.add_page("Project Conclusion", page_conclusion_body)
 
 app.run() # Run the app
